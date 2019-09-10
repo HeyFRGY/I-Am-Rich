47deba3c15664d4da30a548f49a2baa38333c344
Micheal Fergerson <mikefergerson@gmail.com>
2019-09-10 15:49:27 +0900

    initial commit
    The objective of this challenge is to make sure I am comfortable with using the storyboard and designing my own UI elements.

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by AV Japan on 2019/08/29.
+//  Copyright © 2019 FRGY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
